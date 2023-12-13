6d0d49d4526bc6308fb928154269815feb44254f
Ujjwal Kisku <ujjwalkisku14@gmail.com>
2023-12-13 20:01:23 +0530

    fix

--- product-links/navbar-links/navbarLinks.js
+++ product-links/navbar-links/navbarLinks.js
@@ -2,14 +2,18 @@
 export let navbarLinks = [
     {id: 1, name: 'Home', link: '/'},
     {id: 2, name: 'All products', link: '/pages/all-products/allProducts.html'},
-    {id: 3, name: 'Women',
+    {id: 3, name: `Women  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+    <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+  </svg>`,
     products: [
         {id: 1, name: 'All products', link: '/pages/women/all_products/women.html'},
         {id: 2, name: 'Dresses', link: '/pages/women/dresses/dresses.html'},
         {id: 3, name: 'Pants', link: '/pages/women/pants/pants.html'},
         {id: 4, name: 'Skirts', link: '/pages/women/skirts/skirts.html'}
     ]},
-    {id: 4, name: 'Men',
+    {id: 4, name: `Men <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+    <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+  </svg>`,
     products: [
         {id: 1, name: 'All products', link: '/pages/men/all_products/men.html'},
         {id: 2, name: 'Shirts', link: '/pages/men/shirts/shirts.html'},
